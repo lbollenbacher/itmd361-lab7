6aa2d1d10538fefa50575023754f234cb3beb314
Landry Bollenbacher <70623731+lbollenbacher@users.noreply.github.com>
2020-11-29 20:37:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function init(){
 //add your javascrip between these two lines of code
-  document.getElemetbyId("entrybutton").addEventListener("click", f1); 
+  document.getElemetById("entrybutton").addEventListener("click", f1); 
   function f1() {
     var narrowselectors = document.querySelectorAll("h2");
     var selectmyname = narrowselectors.item(0).innerHTML;
