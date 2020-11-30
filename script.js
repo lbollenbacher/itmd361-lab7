789944d92c1140fc159a2bc4819c418ad7b0ada0
Landry Bollenbacher <70623731+lbollenbacher@users.noreply.github.com>
2020-11-29 19:51:42 -0500

    Update script.js
    Since we have looked for all h2 selector, we will be narrowing down further ensure the right content is being called.  We will use .item(0), as all lists start with 0 and then go up from there. The .innerHTML then calls the content within that specific h2 selector. This will display my name when alert/pop-up occurs.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function init(){
   document.getElemetbyId("entrybutton").addEventListener("click", f1); 
   function f1() {
     var narrowselectors = document.querySelectorAll("h2");
+    var selectmyname = narrowselectors.item(0).innerHTML;
   }
 window.addEventListener('load', init);
 }
