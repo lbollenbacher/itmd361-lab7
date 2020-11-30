577ef86e408e2cb2b731d6f88396608b9dab9315
Landry Bollenbacher <70623731+lbollenbacher@users.noreply.github.com>
2020-11-29 19:21:01 -0500

    Update script.js
    Line 4: As described in line 3, we are about to define the function that is going to happen each time entry button is clicked. f1 refers to function 1.

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function init(){
 //add your javascrip between these two lines of code
   document.getElemetbyId("entrybutton").addEventListener("click", f1); 
-
-
-
-
-
-
-
+  function f1() {
+  }
 window.addEventListener('load', init);
+}
