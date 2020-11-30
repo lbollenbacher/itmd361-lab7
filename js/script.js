6dbb19e5b91abc7f4b83eb4af21e77f734134876
Landry Bollenbacher <70623731+lbollenbacher@users.noreply.github.com>
2020-11-29 20:54:30 -0500

    Add files via upload
    Put JavaScript in folder.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function init(){
+//add your javascrip between these two lines of code
+  document.getElemetById("entrybutton").addEventListener("click", f1); 
+  function f1() {
+    var narrowselectors = document.querySelectorAll("h2");
+    var selectmyname = narrowselectors.item(0).innerHTML;
+    var input = document.getElementById("entryinput").value;
+    document.getElementById("textoutput").innerHTML = input;
+    alert(selectmyname + ":" + input);
+  }
+  window.addEventListener('load', init);
+}
