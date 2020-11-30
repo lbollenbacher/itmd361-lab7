e2db436ff38827f55203983d0d149fabe8c7c695
Landry Bollenbacher <70623731+lbollenbacher@users.noreply.github.com>
2020-11-29 20:14:01 -0500

    Update script.js
    The value of the entry input is equal to the value of the output.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function init(){
   function f1() {
     var narrowselectors = document.querySelectorAll("h2");
     var selectmyname = narrowselectors.item(0).innerHTML;
+    var input = document.getElementById("entryinput").value;
+    document.getElementById("textoutput").value = input;
   }
 window.addEventListener('load', init);
 }
