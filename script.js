9041595d5ef19df67da5b9fc06b35e0ecd3424fe
Landry Bollenbacher <70623731+lbollenbacher@users.noreply.github.com>
2020-11-29 19:31:51 -0500

    Update script.js
    Line 5: This pulls all of the contents of the h2 selector (line 11 & 16 in index.html file).

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function init(){
 //add your javascrip between these two lines of code
   document.getElemetbyId("entrybutton").addEventListener("click", f1); 
   function f1() {
+    var narrowselectors = document.querySelectorAll("h2");
   }
 window.addEventListener('load', init);
 }
